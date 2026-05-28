a7ff6444176828ae81b44bc121e94e150cbcc099
Andrés <delaviandres@gmail.com>
2026-05-28 21:24:12 +0200

    quitar un import que no se usaba

--- src/main/java/org/palomafp/apijuegos/api/modelo/Videojuego.java
+++ src/main/java/org/palomafp/apijuegos/api/modelo/Videojuego.java
@@ -3,7 +3,6 @@ package org.palomafp.apijuegos.api.modelo;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
